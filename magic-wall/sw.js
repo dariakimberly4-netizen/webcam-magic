c2828602667a47f830bc6da7f1faa0f571ed6aac
dariakimberly4-netizen <dariakimberly4@gmail.com>
2026-08-11 05:31:08 +0800

    Refresh cache for automatic wall finder

--- magic-wall/sw.js
+++ magic-wall/sw.js
@@ -1,5 +1,5 @@
-const CACHE='kimmy-magic-wall-v9';
-const ASSETS=['./','./index.html','./app.css?v=9','./app.js?v=9','./camera-fix.js?v=9','./wall-anchor.js?v=9','./steady-wall.js?v=9','./manifest.webmanifest','./icon.svg'];
+const CACHE='kimmy-magic-wall-v10';
+const ASSETS=['./','./index.html','./app.css?v=10','./app.js?v=10','./camera-fix.js?v=10','./auto-wall-finder.js?v=10','./steady-wall.js?v=10','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{
