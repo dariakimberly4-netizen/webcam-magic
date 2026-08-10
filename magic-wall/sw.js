e086f8993022c4049518e899e9c3ef733193594a
dariakimberly4-netizen <dariakimberly4@gmail.com>
2026-08-11 05:17:34 +0800

    Refresh Magic Wall cache for steady mode

--- magic-wall/sw.js
+++ magic-wall/sw.js
@@ -1,5 +1,5 @@
-const CACHE='kimmy-magic-wall-v6';
-const ASSETS=['./','./index.html','./app.css?v=6','./app.js?v=6','./camera-fix.js?v=6','./wall-anchor.js?v=6','./manifest.webmanifest','./icon.svg'];
+const CACHE='kimmy-magic-wall-v7';
+const ASSETS=['./','./index.html','./app.css?v=7','./app.js?v=7','./camera-fix.js?v=7','./wall-anchor.js?v=7','./steady-wall.js?v=7','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{
@@ -7,5 +7,5 @@ self.addEventListener('fetch',e=>{
     e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
     return;
   }
-  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request)));
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res;})));
 });
